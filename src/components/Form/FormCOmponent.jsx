6966e6c00fb21d2588968bee07aaa34201e75e08
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-01 17:33:59 +0100

    form validation using formik and yup

--- src/components/Form/FormCOmponent.jsx
+++ src/components/Form/FormCOmponent.jsx
@@ -1,75 +1,64 @@
 import React, { useState } from "react";
-import errorIcon from "../../assets/image/icon-error.svg";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 import "./FormCOmponent.scss";
+import FormElement from "./FormElement";
 
 function FormCOmponent() {
-  const [fnError, setFnError] = useState({ err: false, msg: "" });
-  const [lnError, setLnError] = useState({ err: false, msg: "" });
-  const [eaError, setEaError] = useState({ err: false, msg: "" });
-  const [passError, setPassError] = useState({ err: false, msg: "" });
-  const handelSubmit = (e) => {
-    e.preventDefault();
-
-    let formElem = e.target;
-
-    if (formElem[0].value === "") {
-      return setFnError({ err: true, msg: "First Name cannot be empty" });
-    }
-    setFnError({ err: false, msg: "" });
-    if (formElem[1].value === "") {
-      return setLnError({ err: true, msg: "Last Name cannot be empty" });
-    }
-    setLnError({ err: false, msg: "" });
-    if (formElem[2].value === "") {
-      return setEaError({ err: true, msg: "Email cannot be empty" });
-    } else {
-      if (
-        formElem[2].value.lastIndexOf("@") < 0 ||
-        formElem[2].value.lastIndexOf(".") < 0
-      ) {
-        return setEaError({
-          err: true,
-          msg: "Looks like this is not an email",
-        });
-      }
-    }
-    setEaError({ err: false, msg: "" });
-    if (formElem[3].value === "") {
-      return setPassError({ err: true, msg: "Password cannot be empty" });
-    }
-    setPassError({ err: false, msg: "" });
+  const validate = Yup.object({
+    firstName: Yup.string().required("First Name cannot be empty"),
+    lastName: Yup.string().required("Last Name cannot be empty"),
+    email: Yup.string()
+      .email("Looks like this is not an email")
+      .required("Email cannot be empty"),
+    password: Yup.string().required("Password cannot be empty"),
+  });
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  };
+  const handelSubmit = (formData) => {
+    console.log(formData);
   };
   return (
     <div className="FormCOmponent">
       <div className="top-card">Try it free 7 days then $20/mo. thereafter</div>
       <div className="form-card">
-        <form onSubmit={(e) => handelSubmit(e)}>
-          <label className={`input-div ${fnError.err ? "error" : null}`}>
-            <input type="text" placeholder="First Name" />{" "}
-            {fnError.err ? <img src={errorIcon} alt="" /> : null}
-          </label>
-          <sup className="error">{fnError.err ? fnError.msg : null}</sup>
-          <label className={`input-div ${lnError.err ? "error" : null}`}>
-            <input type="text" placeholder="Last Name" />
-            {lnError.err ? <img src={errorIcon} alt="" /> : null}
-          </label>
-          <sup className="error">{lnError.err ? lnError.msg : null}</sup>
-          <label className={`input-div ${eaError.err ? "error" : null}`}>
-            <input type="" placeholder="Email Address" />
-            {eaError.err ? <img src={errorIcon} alt="" /> : null}
-          </label>
-          <sup className="error">{eaError.err ? eaError.msg : null}</sup>
-          <label className={`input-div ${passError.err ? "error" : null}`}>
-            <input type="password" placeholder="Password" />
-            {passError.err ? <img src={errorIcon} alt="" /> : null}
-          </label>
-          <sup className="error">{passError.err ? passError.msg : null}</sup>
-          <button>Claim your free trial </button>
-          <sup>
-            By clicking the button, you are agreeing to our
-            <a href="#">Terms and Services</a>
-          </sup>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validate}
+          onSubmit={(FormData) => handelSubmit(FormData)}
+        >
+          {(Formik) => (
+            <>
+              <Form>
+                <FormElement
+                  type="text"
+                  label={"First Name"}
+                  name={"firstName"}
+                />
+                <FormElement
+                  type="text"
+                  label={"Last Name"}
+                  name={"lastName"}
+                />
+                <FormElement type="email" label={"Email"} name={"email"} />
+                <FormElement
+                  type="password"
+                  label={"Password"}
+                  name={"password"}
+                />
+                <button type="submit">Claim your free trial </button>
+              </Form>
+              <sup>
+                By clicking the button, you are agreeing to our
+                <a href="#">Terms and Services</a>
+              </sup>
+            </>
+          )}
+        </Formik>
       </div>
     </div>
     // </div>
